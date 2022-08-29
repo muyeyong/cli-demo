2aaa915512f916742ad3ad609a0c9c6c0cecabe3
muyeyong <xuyongshuaige@gmail.com>
2022-08-29 23:47:35 +0800

    feat: 获取仓库失败了

--- README.md
+++ README.md
@@ -6,4 +6,7 @@
 commander 就是提供帮助命令的吗？
 项目里面同时用 require 和 import引入，混合使用 or 强制使用某种引入
 
+npm unlink lvluo-cli 不生效 ===> npm r lvluo-cli -g       
+     
+
 
--- cli.js
+++ cli.js
@@ -1,46 +1,15 @@
 #! /usr/bin/env node
 const commander = require('commander')
-const chalk = require('chalk')
-// import inquirer from 'inquirer'
 const inquirer = require('inquirer')
+const ora = require('ora')
 
-// // console.log(chalk.blue('lvluo-cli'))
-
-inquirer.prompt([
-  {
-    name: "vue",
-    // 多选交互功能
-    // 单选将这里修改为 list 即可
-    type: "checkbox",
-    message: "Check the features needed for your project:",
-    choices: [
-      {
-        name: "Babel",
-        checked: true,
-      },
-      {
-        name: "TypeScript",
-      },
-      {
-        name: "Progressive Web App (PWA) Support",
-      },
-      {
-        name: "Router",
-      },
-    ],
-  },
-]).then((data) => {
-  console.log(data);
+commander.command("create <project-name>") // 增加创建指令
+.description("create a new project") // 添加描述信息
+.option("-f, --force", "overwrite target directory if it exists") // 强制覆盖
+.action((projectName, cmd) => {
+  // 处理用户输入create 指令附加的参数
+  require('./lib/create')(projectName, cmd)
 })
 
-commander.name('lvluo-cli').usage('<command> [option]').version('1.0.0')
-commander.parse(process.argv)
-
-
-// const a = require('./a.js')
-// const b = require('./b.js').default
-// import a from './a.js'
-// import b from './b.js'
-
-// a()
-// b()
+commander.name('lvluo-cli').usage('<command> [option]').version(`lvluo-cli ${require('./package.json').version}`)
+commander.parse(process.argv);
