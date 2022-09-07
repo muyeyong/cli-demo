097d83ef4345ae409845979d99f5efc1e99f0121
muyeyong <xuyongshuaige@gmail.com>
2022-09-07 23:50:55 +0800

    引入模板引擎

--- cli.js
+++ cli.js
@@ -1,9 +1,11 @@
 #! /usr/bin/env node
-const commander = require('commander')
+const program = require('commander')
 const inquirer = require('inquirer')
 const ora = require('ora')
+const ejs = require('ejs')
+const path = require('path')
 
-commander.command("create <project-name>") // 增加创建指令
+program.command("create <project-name>") // 增加创建指令
 .description("create a new project") // 添加描述信息
 .option("-f, --force", "overwrite target directory if it exists") // 强制覆盖
 .action((projectName, cmd) => {
@@ -11,5 +13,21 @@ commander.command("create <project-name>") // 增加创建指令
   require('./lib/create')(projectName, cmd)
 })
 
-commander.name('lvluo-cli').usage('<command> [option]').version(`lvluo-cli ${require('./package.json').version}`)
-commander.parse(process.argv);
+program.command("addCmp <component-name>")
+.description("create a new component")
+.option("-d, --dir", "specifies directory creation")
+.action((name) => {
+  console.log(path.join(__dirname, '/template/vue.ejs'))
+  // ejs.renderFile(path.join(__dirname, '/template/vue.ejs'), { name }, {}, (err, str) => {
+  //   console.log('ejs result', str)
+  // })
+  // let people = ['geddy', 'neil', 'alex']
+  // html = ejs.render('<%= name %>', {name: name});
+  // console.log('html', html)
+ const html1 =  ejs.renderFile(path.join(__dirname, '/template/vue.ejs'), { data: { name }})
+ console.log(html1)
+  // const t = ejs.render('<=% name %>', { name })
+  // console.log('t', t)
+})
+program.name('lvluo-cli').usage('<command> [option]').version(`lvluo-cli ${require('./package.json').version}`)
+program.parse(process.argv);
